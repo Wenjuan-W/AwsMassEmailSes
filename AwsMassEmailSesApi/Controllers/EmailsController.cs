a2ce56c09452cb9a3afb5343279577bbd1842231
wenjuan-w <43392440+Wenjuan-W@users.noreply.github.com>
2025-01-10 21:28:01 -0500

    Add files via upload
    Initial commit

---
+++ AwsMassEmailSesApi/Controllers/EmailsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AwsMassEmailSesApi.Controllers
+{
+    public class EmailsController : Controller
+    {
+        public IActionResult GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> SendMassEmail(IFormFile emailCsvFile)
+        {
+            // takes in CSV and uploads to S3
+
+            // validate that file is of type csv
+            // check that input contains only valid email address
+            // call service to upload csv to s3
+
+            // response will be http response with any exceptions from s3 
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AwsMassEmailSesApi/Services/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using AwsMassEmailSesApi.Contracts.Models;
+using AwsMassEmailSesApi.Contracts.Services;
+using System.Text;
+
+namespace AwsMassEmailSesApi.Services
+{
+    public class EmailService : IEmailService
+    {
+        public IFormFile GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<S3UploadResponse> UploadEmailCsv(IFormFile csvFile, string bucketName)
+        {
+            IAmazonS3 client = new AmazonS3Client();
+            string csvFileName = csvFile.FileName;
+
+            try
+            {
+
+                await client.UploadObjectFromStreamAsync(bucketName, csvFileName, csvFile.OpenReadStream(), new Dictionary<string, object>());
+            }
+            catch (Exception ex) 
+            { 
+                
+            }
+
+            return new S3UploadResponse();
+        }
+    }
+}
